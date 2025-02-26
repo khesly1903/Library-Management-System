56a0c5f2aebb939d6d9327797a0f5cc3e98a6585
berkay <kayaberkay1626@gmail.com>
2025-02-26 12:06:59 +0300

    abstractions and entitiy v1

--- libraryProject.Business/Abstractions/IManager.cs
+++ libraryProject.Business/Abstractions/IManager.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace libraryProject.Business.Abstractions
 {
-    internal interface IManager
+    public interface IManager<T> where T : class
     {
+        void Create(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        T GetById(Guid id);
+        IEnumerable<T> GetAll();
+
+
+        bool IfEntityExists(Expression<Func<T,bool>> fiter);
     }
 }
--- libraryProject.Entities/Models/Author.cs
+++ libraryProject.Entities/Models/Author.cs
@@ -1,12 +1,18 @@
-﻿using System;
+﻿using libraryProject.Entities.Abstractions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace libraryProject.Entities.Models
 {
-    internal class Author
+    public class Author: Entity
     {
+        public string? Name { get; set; }
+        public string? Surname { get; set; }
+
+        public string? Biography{ get; set; }
     }
 }
