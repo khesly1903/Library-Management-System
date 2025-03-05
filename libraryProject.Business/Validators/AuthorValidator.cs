8f51ba9041d8d0ac9e2db4d16125e5d8f7209de7
khesly1903 <kayaberkay1626@gmail.com>
2025-03-05 11:49:49 +0300

    yazar validasyonu güncellendi

--- libraryProject.Business/Validators/AuthorValidator.cs
+++ libraryProject.Business/Validators/AuthorValidator.cs
@@ -9,11 +9,13 @@ namespace libraryProject.Business.Validators
 
             RuleFor(a => a.Name)
           .NotEmpty().WithMessage("Yazar adı boş olamaz.")
-          .MinimumLength(2).WithMessage("Yazar adı en az 2 karakter olmalıdır.");
+          .MinimumLength(2).WithMessage("Yazar adı en az 2 karakter olmalıdır.")
+           .Matches(@"^[a-zA-ZÇİÜĞŞÖçüğşöı\s]+$").WithMessage("Yazar adı sadece harf içerebilir, sayı ve özel karakter içeremez.");
 
             RuleFor(a => a.Surname)
            .NotEmpty().WithMessage("Yazar soyadı boş olamaz.")
-           .MinimumLength(2).WithMessage("Yazar soyadı en az 2 karakter olmalıdır.");
+           .MinimumLength(2).WithMessage("Yazar soyadı en az 2 karakter olmalıdır.")
+           .Matches(@"^[a-zA-ZÇİÜĞŞÖçüğşöı\s]+$").WithMessage("Yazar soyadı sadece harf içerebilir, sayı ve özel karakter içeremez.");
 
             RuleFor(a => a.Biography1)
            .MaximumLength(1000).WithMessage("Biyografi 1000 karakterden uzun olamaz.");
